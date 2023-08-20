3e385fcc1240168b79774996c827baec1b3bac61
Tobechukwu Ezenachukwu <ebot64@yahoo.com>
2023-08-20 20:08:40 +0100

    Data validation added

--- src/mlProject/pipeline/stage_02_data_validation.py
+++ src/mlProject/pipeline/stage_02_data_validation.py
@@ -1,11 +1,10 @@
 from mlProject.config.configuration import ConfigurationManager
-from mlProject.components.data_ingestion import DataIngestion
+from mlProject.components.data_validation import DataValidation
 from mlProject import logger
 
+STAGE_NAME = "Data Validation stage"
 
-STAGE_NAME = "Data Ingestion stage"
-
-class DataIngestionTrainingPipeline:
+class DataValidationTrainingPipeline:
     def __init__(self):
         pass
         """ self.config = config
@@ -13,16 +12,15 @@ class DataIngestionTrainingPipeline:
 
     def main(self):
         config = ConfigurationManager()
-        data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config=data_ingestion_config)
-        data_ingestion.download_file()
-        data_ingestion.extract_zip_file()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
 
 
 if __name__ == "__main__":
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
+        obj = DataValidationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
